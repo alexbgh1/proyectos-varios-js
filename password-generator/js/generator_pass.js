4a832e24a0ca119b09c29181aca85fed01a3d1f7
alexbgh1 <alex.garnica@alumnos.uach.cl>
2022-08-18 19:54:16 -0400

    generator_pass -> clipboard

--- password-generator/js/generator_pass.js
+++ password-generator/js/generator_pass.js
@@ -3,3 +3,15 @@ const lowerLetters = "abcdefghijlkmnñoprqstuvwxyz"
 const upperLetters = "ABCDEFGHIJLKMNÑOPRQSTUVWXYZ"
 const symbols = "|@#~€¬!$%&/()=+-ºª*"
 
+var inputPass = document.getElementById("gen-pass");
+var tooltip = document.getElementById("copyclip");
+
+function copyPass(){
+    inputPass.select();
+    inputPass.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(inputPass.value);
+    tooltip.innerHTML = "Copied!";
+}
+function outMsgCopyclip(){
+    tooltip.innerHTML = "Copy to clipboard";
+}
