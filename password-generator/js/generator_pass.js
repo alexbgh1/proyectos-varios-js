3777ec8c2502a15e4988dc8f06ea800734575353
alexbgh1 <alex.garnica@alumnos.uach.cl>
2022-08-20 02:12:56 -0400

    avance generator_pass; mejorar aspectos visuales

--- password-generator/js/generator_pass.js
+++ password-generator/js/generator_pass.js
@@ -1,11 +1,20 @@
+
 const numbers = "0123456789"
 const lowerLetters = "abcdefghijlkmnñoprqstuvwxyz"
 const upperLetters = "ABCDEFGHIJLKMNÑOPRQSTUVWXYZ"
 const symbols = "|@#~€¬!$%&/()=+-ºª*"
 
-var inputPass = document.getElementById("gen-pass");
-var tooltip = document.getElementById("copyclip");
+/* GET ID */
+const inputPass = document.getElementById("gen-pass");
+const tooltip = document.getElementById("copyclip");
+const length_num = document.getElementById('length-num');
+const num = document.getElementById('cb-num');
+const ll = document.getElementById('cb-ll');
+const ul = document.getElementById('cb-ul');
+const s = document.getElementById('cb-s');
 
+var checkboxes = document.querySelectorAll('.cb')
+/* Copy Clip feature */
 function copyPass(){
     inputPass.select();
     inputPass.setSelectionRange(0, 99999);
@@ -14,4 +23,38 @@ function copyPass(){
 }
 function outMsgCopyclip(){
     tooltip.innerHTML = "Copy to clipboard";
+}
+
+/* stepper */
+function stepper(btn){
+    let id = btn.getAttribute('id');
+    let min = length_num.getAttribute('min');
+    let max = length_num.getAttribute('max');
+    let val = length_num.getAttribute('value');
+
+    let calcStep = (id == "increment") ? 1 : -1;
+    let newValue = parseInt(val) + calcStep;
+
+    if (newValue >= min && newValue <= max){
+        length_num.setAttribute('value', newValue);
+    }
+}
+
+/* Pass generator */
+function getInfo(){
+    checkboxes
+    var posibleChar = ""
+    if (checkboxes[0].checked){posibleChar+=numbers}
+    if (checkboxes[1].checked){posibleChar+=lowerLetters}
+    if (checkboxes[2].checked){posibleChar+=upperLetters}
+    if (checkboxes[3].checked){posibleChar+=symbols}
+    var ans = ""
+    if (posibleChar === ""){
+        ans="Please check some values";
+    }else{
+        for(let l = 0; l<length_num.value; l++){
+            ans += posibleChar[(Math.floor(Math.random() * posibleChar.length))];
+        };
+    }
+    inputPass.value = ans;
 }
