7ba708078a3f60eb5068ea19bdd5b594d5b08066
alexbgh1 <alex.garnica@alumnos.uach.cl>
2022-08-21 00:12:49 -0400

    plantilla

---
+++ sort/js/script.js
@@ -0,0 +1,8 @@
+const inputBox = document.getElementById('sort-input');
+const sortBox = document.getElementById('sort-bar');
+
+console.log(inputBox);
+console.log(sortBox);
+sortBox.innerHTML = `
+<div class="bar">
+</div>`
