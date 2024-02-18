0424a335793a4b9504f84dc4bc43c17fa688d582
alexbgh1 <82188424+alexbgh1@users.noreply.github.com>
2024-02-17 22:54:45 -0300

    Delete sort directory

--- sort/js/script.js
+++
@@ -1,8 +0,0 @@
-const inputBox = document.getElementById('sort-input');
-const sortBox = document.getElementById('sort-bar');
-
-console.log(inputBox);
-console.log(sortBox);
-sortBox.innerHTML = `
-<div class="bar">
-</div>`
